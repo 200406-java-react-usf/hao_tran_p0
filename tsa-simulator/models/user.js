a068f01ffb10fea5262bfaf903de6ba6807082d1
donxiya <31514669+donxiya@users.noreply.github.com>
2020-04-21 21:27:41 -0500

    mvp version

---
+++ tsa-simulator/models/user.js
@@ -0,0 +1,14 @@
+const pg = require("pg");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    attempts: { type: String, required: true },
+    rank: { type: String, required: true },
+});
+
+const user = pg.model("User", userSchema);
+
+module.exports = user;
